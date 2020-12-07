c7ba56d4ba5d72f905d60943ecfbc041afba6f75
AnnWozniak <70873524+AnnWozniak@users.noreply.github.com>
2020-12-06 17:30:56 -0700

    Add files via upload

---
+++ Manuscript/Manuscript.R
@@ -0,0 +1,39 @@
+## ----Table 1, echo=FALSE, table.cap="Terms and Descriptions", table.show="asis", out.width = '100%'----
+knitr::include_graphics("C:/Users/annwozniak/Desktop/EEB_Final_Project_Wozniak/Figures/Terms_and_descriptions.png")
+
+
+## ----fig.1, echo=FALSE, fig.cap="Domicile type by health (1-5 scale, good to poor)", fig.show="asis", out.width = '100%'----
+knitr::include_graphics("C:/Users/annwozniak/Desktop/EEB_Final_Project_Wozniak/Figures/graph_violin_Domicile_by_Health.png")
+
+
+## ----fig.2, echo=FALSE, fig.cap="Domicile type by happiness (0-10 scale, unhappy to happy)", fig.show="asis", out.width = '100%'----
+knitr::include_graphics("C:/Users/annwozniak/Desktop/EEB_Final_Project_Wozniak/Figures/graph_violin_Domicile_by_Happiness.png")
+
+
+## ----fig.3, echo=FALSE, fig.cap="Domicile type by social (0-7 scale, never to daily)", fig.show="asis", out.width = '100%'----
+knitr::include_graphics("C:/Users/annwozniak/Desktop/EEB_Final_Project_Wozniak/Figures/graph_violin_Domicile_by_Social.png")
+
+
+## ----fig.4, echo=FALSE, fig.cap="Social connectedness by health linear model", fig.show="asis", out.width = '100%'----
+knitr::include_graphics("C:/Users/annwozniak/Desktop/EEB_Final_Project_Wozniak/Figures/graph_linear_Social_by_Health.png")
+
+
+## ----fig.5, echo=FALSE, fig.cap="Happiness by health linear model ", fig.show="asis", out.width = '100%'----
+knitr::include_graphics("C:/Users/annwozniak/Desktop/EEB_Final_Project_Wozniak/Figures/graph_linear_Happy_by_Health.png")
+
+
+## ----fig.6, echo=FALSE, fig.cap="Species-Individual Curve Plot", fig.show="asis", out.width = '100%'----
+knitr::include_graphics("C:/Users/annwozniak/Desktop/EEB_Final_Project_Wozniak/Figures/Species-individual_curve2.png")
+
+
+## ----fig.7, echo=FALSE, fig.cap="(a) Effective number of species by domicile type,  (b) Effective number of species by happiness (0-10 scale, unhappy to happy), (c) Effective number of species by health (1-5 scale, good to poor), and (d) Effective number of species by social connectedness (0-7 scale, never to daily)", fig.show="asis", out.width = '100%'----
+knitr::include_graphics("C:/Users/annwozniak/Desktop/EEB_Final_Project_Wozniak/Figures/Effective_number_of_species_by_metadata2.png")
+
+
+## ----fig.8, echo=FALSE, fig.cap="Bray-Curtis NMDS", fig.show="asis", out.width = '100%'----
+knitr::include_graphics("C:/Users/annwozniak/Desktop/EEB_Final_Project_Wozniak/Figures/Bray-Curtis.png")
+
+
+## ----fig.9, echo=FALSE, fig.cap="Jaccard NMDS", fig.show="asis", out.width = '100%'----
+knitr::include_graphics("C:/Users/annwozniak/Desktop/EEB_Final_Project_Wozniak/Figures/Jaccard.png")
+
